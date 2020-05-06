205d061c7aeb3d8080a3c8cb809b525b101c1d93
deyems <adeyemisola11@gmail.com>
2020-05-06 01:58:51 +0100

    Infinite Scroll App

---
+++ script.js
@@ -0,0 +1,12 @@
+class InfiniteScrollPost {
+  constructor() {
+    //CALL YOUR DOM OBJECTS HERE
+    this.postsContainer = document.getElement
+  }
+  run(){
+    //LISTEN TO EVENTS HERE
+  }
+}
+
+let caller = new ClassName();
+caller.run();
